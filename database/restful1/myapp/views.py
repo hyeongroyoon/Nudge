6d3b4a13ac8fa42fde1438bb9fcd4366881c8abb
Yoon <dbsgudfh94@gmail.com>
2019-05-17 20:23:16 +0900

    remove a directory

--- database/restful1/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from myapp.serializers import FoodSerializer
-from myapp.models import *
-
-# Create your views here.
-class FoodViewSet(viewsets.ModelViewSet):
-    queryset = Menu.objects.all()
-    serializer_class = FoodSerializer
